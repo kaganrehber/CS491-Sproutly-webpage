8daa8f7f1e6ce3f37c21e4d62dfb63346af9a0a8
Kağan Rehber <kagan.rehber@ug.bilkent.edu.tr>
2026-03-22 17:56:04 +0300

    New report added

--- sproutly-web/src/App.jsx
+++ sproutly-web/src/App.jsx
@@ -1,5 +1,13 @@
 import "./index.css";
 
+var logbooks = [
+  "https://docs.google.com/document/d/1b4NXpplILX89qYNhKv6EYOnXRQ_W1Ar9LE8pEZ6dw88/edit?usp=sharing",
+  "https://docs.google.com/document/d/1XuOBwZaCWMEw83P3PU5MEZLREvLD8WJFGVngF3p8siU/edit?usp=sharing",
+  "https://docs.google.com/document/d/1YJlnnLuDt0qbw9eZplZqHLV1DCKhdltG-Bs9iCpaSnM/edit?usp=sharing",
+  "https://docs.google.com/document/d/1bC7BRaHLyj_JbQlnGuOT_ldmgaa15NcxbUesIrY6IwY/edit?usp=sharing",
+  "https://docs.google.com/document/d/19GZoMgb3bn0vPvAdjrIAEjUee1N4e3BaHTXGWuE1uXU/edit?usp=sharing",
+];
+
 function App() {
   return (
     <div className="w-full min-h-screen font-sans">
@@ -24,6 +32,12 @@ function App() {
             className="text-white hover:text-gray-100 transition-colors cursor-pointer"
           >
             Our Team
+          </a>
+          <a
+            href="#documents"
+            className="text-white hover:text-gray-100 transition-colors cursor-pointer"
+          >
+            Documents
           </a>
         </nav>
       </header>
@@ -155,6 +169,123 @@ function App() {
                 </p>
               </div>
             </div>
+          </div>
+        </section>
+
+        <section
+          id="documents"
+          className="w-full min-h-screen flex flex-row items-center justify-center bg-white p-8"
+        >
+          <div id="docs" className="flex flex-col m-10">
+            <h2 className="text-3xl font-bold mb-8 text-[#056d52]">
+              Documents
+            </h2>
+
+            <ul className="w-full max-w-xl space-y-4">
+              <li className="bg-[#56daae]/10 hover:bg-[#56daae]/20 transition rounded-lg shadow p-4">
+                <a
+                  href="documents/project-proposal.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#056d52] font-semibold text-lg"
+                >
+                  📄 Project Proposal
+                </a>
+              </li>
+
+              <li className="bg-[#56daae]/10 hover:bg-[#56daae]/20 transition rounded-lg shadow p-4">
+                <a
+                  href="documents/project-specification.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#056d52] font-semibold text-lg"
+                >
+                  📄 Project Specification Report
+                </a>
+              </li>
+
+              <li className="bg-[#56daae]/10 hover:bg-[#56daae]/20 transition rounded-lg shadow p-4">
+                <a
+                  href="documents/analysis-requirements.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#056d52] font-semibold text-lg"
+                >
+                  📄 Analysis and Requirements Report
+                </a>
+              </li>
+
+              <li className="bg-[#56daae]/10 hover:bg-[#56daae]/20 transition rounded-lg shadow p-4">
+                <a
+                  href="documents/detailed-design.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#056d52] font-semibold text-lg"
+                >
+                  📄 Detailed Design Report
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div id="docs" className="flex flex-col m-10">
+            <h2 className="text-3xl font-bold mb-8 text-[#056d52]">Logbooks</h2>
+
+            <ul className="w-full max-w-xl space-y-4">
+              <li className="bg-[#56daae]/10 hover:bg-[#56daae]/20 transition rounded-lg shadow p-4">
+                <a
+                  href={logbooks[0]}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#056d52] font-semibold text-lg"
+                >
+                  📄Kağan Rehber - Logbook
+                </a>
+              </li>
+
+              <li className="bg-[#56daae]/10 hover:bg-[#56daae]/20 transition rounded-lg shadow p-4">
+                <a
+                  href={logbooks[1]}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#056d52] font-semibold text-lg"
+                >
+                  📄 Bilge İdil Öziş - Logbook
+                </a>
+              </li>
+
+              <li className="bg-[#56daae]/10 hover:bg-[#56daae]/20 transition rounded-lg shadow p-4">
+                <a
+                  href={logbooks[2]}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#056d52] font-semibold text-lg"
+                >
+                  📄 Mennatallah Abouelenin - Logbook
+                </a>
+              </li>
+
+              <li className="bg-[#56daae]/10 hover:bg-[#56daae]/20 transition rounded-lg shadow p-4">
+                <a
+                  href={logbooks[3]}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#056d52] font-semibold text-lg"
+                >
+                  📄 Elif Ercan - Logbook
+                </a>
+              </li>
+
+              <li className="bg-[#56daae]/10 hover:bg-[#56daae]/20 transition rounded-lg shadow p-4">
+                <a
+                  href={logbooks[4]}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[#056d52] font-semibold text-lg"
+                >
+                  📄 Bilgehan Tuğcu - Logbook
+                </a>
+              </li>
+            </ul>
           </div>
         </section>
       </main>
